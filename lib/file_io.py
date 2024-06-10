3039cc90e0a189b31fb0c7c8e52e72d92175afdb
+ <+>
2024-06-10 13:47:01 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(f"\n{append_content}")
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as file:
+        content = file.read()
+    return contentwrite_file("logfile", "Log 1: 5 bananas added")
+    append_file("logfile", "Log 2: 3 bananas subtracted")
+    
+    print(read_file("logfile"))
